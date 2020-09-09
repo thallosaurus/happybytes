c30f506d9fbec6166975f08743ad49cebf678297
thallosaurus <activator112233@gmail.com>
2020-09-09 18:56:39 +0200

    Fixed a bug

--- happybytes.js
+++ happybytes.js
@@ -1,7 +1,7 @@
 /**
  * Takes a size in bytes and converts it to the correct multiples of bytes and appends the right unit
  *
- * @param {number} size Filesize in Bytes
+ * @param {number} [size=0] Filesize in Bytes
  * @param {boolean} [useMiB=false] If true, uses a base of 1024 (KB, GB...) instead of 1000 (KiB, GiB...)
  * @returns String
  */
@@ -10,19 +10,20 @@ function happybytes(size = 0, useMiB = false)
     let unit = [
         ["B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"],
         ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
-    ];
+    ][useMiB ? 0 : 1];
+
     let base = useMiB ? 1024 : 1000;
     let c = 0;
     do {
         size /= base;
         c++;
-        if (size < base)
+        if (size < base || c >= unit.length - 1)
         {
             break;
         }
     } while (size > base);
 
-    return new String(Math.round((size * 100)) / 100 + unit[useMiB ? 0 : 1][c]);
+    return String(Math.floor(Math.round((size * 100))) / 100 + unit[c]);
 }
 
 module.exports = happybytes;
--- tests.js
+++ tests.js
@@ -1,6 +1,6 @@
 const happybytes = require("./happybytes.js");
 
-let a = [1337, 1333, 85869345, 33305495, 1, 55555];   //throw as many sizes as you like in here, of course in bytes
+let a = [99999999999999999999999999999999999999, 1337, 1333, 85869345, 33305495, 1, 55555];   //throw as many sizes as you like in here, of course in bytes
 
 for (let b of a)
 {
