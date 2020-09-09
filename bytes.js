0da3b79bd390f9533d20fbdb69360eadd0e2a5b8
Philipp Gaßner <activator112233@gmail.com>
2020-09-09 16:07:18 +0200

    Added files

---
+++ bytes.js
@@ -0,0 +1,28 @@
+/**
+ * Takes a size in bytes and converts it to the correct multiples of bytes and appends the right unit
+ *
+ * @param {number} size Filesize in Bytes
+ * @param {boolean} [useMiB=false] If true, uses a base of 1024 (KB, GB...) instead of 1000 (KiB, GiB...)
+ * @returns String
+ */
+function sizeAsString(size = 0, useMiB = false)
+{
+    let unit = [
+        ["B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"],
+        ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
+    ];
+    let base = useMiB ? 1024 : 1000;
+    let c = 0;
+    do {
+        size /= base;
+        c++;
+        if (size < base)
+        {
+            break;
+        }
+    } while (size > base);
+
+    return new String(Math.round((size * 100)) / 100 + unit[useMiB ? 0 : 1][c]);
+}
+
+module.exports = sizeAsString;
---
+++ tests.js
@@ -0,0 +1,8 @@
+const bytes = require("./bytes.js");
+
+let a = [1333, 85869345, 33305495, 1, 55555];   //throw as many sizes as you like in here, of course in bytes
+
+for (let b of a)
+{
+    console.log(bytes(b));
+}
