8ea2c0d6800ef66e5775f5b35933728cc2a89f1c
Philipp Gaßner <activator112233@gmail.com>
2020-09-09 19:09:44 +0200

    Delete tests.js

--- tests.js
+++
@@ -1,8 +0,0 @@
-const happybytes = require("./happybytes.js");
-
-let a = [99999999999999999999999999999999999999, 1337, 1333, 85869345, 33305495, 1, 55555];   //throw as many sizes as you like in here, of course in bytes
-
-for (let b of a)
-{
-    console.log(happybytes(b));
-}
