eb7030b106b4d0c7105156a24204171f47c4ed52
Philipp Gaßner <activator112233@gmail.com>
2020-09-09 16:26:34 +0200

    Update tests.js

--- tests.js
+++ tests.js
@@ -1,8 +1,8 @@
-const bytes = require("./bytes.js");
+const happybytes = require("./happybytes.js");
 
 let a = [1333, 85869345, 33305495, 1, 55555];   //throw as many sizes as you like in here, of course in bytes
 
 for (let b of a)
 {
-    console.log(bytes(b));
+    console.log(happybytes(b));
 }
